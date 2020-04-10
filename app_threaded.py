f5ee9fdc77a8cb226f85ef7d5ed0f31a3190e795
pravid <truptipawar.developer@gmail.com>
2020-04-11 00:05:26 +0530

    app_thraded update

--- app_threaded.py
+++ app_threaded.py
@@ -116,7 +116,8 @@ def gen(camera):
 
 					# * ---------- SEND data to API --------- *
 
-					r = requests.post(url='http://127.0.0.1:5000/receive_data', json=json_to_export)
+					#r = requests.post(url='http://127.0.0.1:5000/receive_data', json=json_to_export)
+					r = requests.post(url='https://testfacedetect.herokuapp.com/receive_data', json=json_to_export)
 					print("Status: ", r.status_code)
 
 				face_names.append(name)
